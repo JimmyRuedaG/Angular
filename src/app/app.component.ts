11706afefb148c30b4fe815ca82060aa0c90d795
JimmyRuedaG <jimmy960711@hotmail.com>
2021-09-10 07:53:17 -0500

    subiendo uno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public title = 'lineaIII';
+  public title = 'primer programa angular';
+  n1 : number;
+  n2:number;
+  resultado:number;
+
+  public operar(op:string):void{
+switch(op){
+  case 's':
+    this.resultado = this.n1 + this.n2;
+    break;
+  case 'r':
+    this.resultado = this.n1 - this.n2;
+    break
+  case 'm':
+    this.resultado = this.n1 * this.n2;
+    break;
+  case 'd':
+    this.resultado = this.n1 / this.n2;
+    break;
+  default:
+    this.resultado = 0;
+}
+
+  }
+
+
 }
